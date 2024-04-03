dd90e45c07de6d3434818621aa10f3ed3b590a31
bedialp <fvzlpktk@gmail.com>
2024-04-03 09:22:27 +0300

    Add project files.

---
+++ LearnerProject/Models/Entities/Banner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearnerProject.Models.Entities
+{
+    public class Banner
+    {
+        public int BannerId { get; set; }
+        public String Title { get; set; }
+
+    }
+}
