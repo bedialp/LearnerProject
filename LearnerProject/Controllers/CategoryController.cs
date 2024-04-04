5dca3bf3bfbdb88de8a97e2dd09841e275c97957
bedialp <fvzlpktk@gmail.com>
2024-04-04 23:05:10 +0300

    2.Ders

---
+++ LearnerProject/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using LearnerProject.Models.Context;
+using LearnerProject.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnerProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        LearnerContext context = new LearnerContext();
+        // GET: Category
+        public ActionResult Index()
+        {
+            var values = context.Categories.Where(x => x.Status == true).ToList();
+            return View(values);
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            value.Status = false;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // KATEGORI EKLEME ISLEMI
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            category.Status = true;
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // KATEGORI GUNCELLEME ISLEMI
+        public ActionResult UpdateCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            var value = context.Categories.Find(category.CategoryId);
+            value.CategoryName = category.CategoryName;
+            value.Icon=category.Icon;
+            value.Status=category.Status;
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
