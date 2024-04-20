03c5c9c99f5bb7cac6410212615d8de161e46b7b
bedialp <fvzlpktk@gmail.com>
2024-04-20 23:44:23 +0300

    Default View and Controller Added

--- LearnerProject/Controllers/DefaultController.cs
+++ LearnerProject/Controllers/DefaultController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using LearnerProject.Models;
+using LearnerProject.Models.Context;
+using LearnerProject.Models.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +12,26 @@ namespace LearnerProject.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+        LearnerContext context = new LearnerContext();
         public ActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult DefaultCoursePartial()
+        {
+
+            var values = context.Courses.Include(x => x.Reviews).OrderByDescending(x => x.CourseId).Take(3).ToList();
+
+            return PartialView(values);
+        }
+
+        public ActionResult CourseDetail(int id)
+        {
+            var values = context.Courses.Find(id); // Veritabanından belirtilen ID'ye sahip kurs bilgileri alınır
+            var reviewList = context.Reviews.Where(x => x.CourseId == id).ToList(); // Belirtilen kursa ait incelemeler alınır
+            ViewBag.review = reviewList; // Incelemeleri ViewBag aracılığıyla View'a gönderilir
+            return View(values);
         }
     }
 }
