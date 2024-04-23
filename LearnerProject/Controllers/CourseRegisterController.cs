0eb72e264fdfed39309183a10de71f152f45b679
bedialp <fvzlpktk@gmail.com>
2024-04-23 22:38:48 +0300

    CourseRegister Added

--- LearnerProject/Controllers/CourseRegisterController.cs
+++ LearnerProject/Controllers/CourseRegisterController.cs
@@ -1,5 +1,4 @@
-﻿// using LearnerProject.Models.Class;
-using LearnerProject.Models.Context;
+﻿using LearnerProject.Models.Context;
 using LearnerProject.Models.Entities;
 using System;
 using System.Collections.Generic;
@@ -12,89 +11,25 @@ namespace LearnerProject.Controllers
     public class CourseRegisterController : Controller
     {
         LearnerContext context = new LearnerContext();
-        //  IEClass ie = new IEClass();
 
+        [HttpGet]
         public ActionResult Index()
         {
-            var values = context.CourseRegisters.ToList();
-            return View(values);
-        }
-
-        // KURS KAYDI SILME ISLEMI
-        public ActionResult DeleteCourseRegister(int id)
-        {
-            var value = context.CourseRegisters.Find(id);
-            context.CourseRegisters.Remove(value);
-            context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        // KURS KAYDI EKLEME ISLEMI
-        [HttpGet]
-        public ActionResult AddCourseRegister()
-        {
-            var courses = context.Courses.ToList();
-            List<SelectListItem> courseList = (from y in courses
-                                               select new SelectListItem
-                                               {
-                                                   Text = y.CourseName,
-                                                   Value = y.CourseId.ToString()
-                                               }).ToList();
-            ViewBag.course = courseList;
-
-            var students = context.Students.ToList();
-            List<SelectListItem> studentList = (from x in students
-                                                select new SelectListItem
-                                                {
-                                                    Text = x.UserName,
-                                                    Value = x.StudentId.ToString()
-                                                }).ToList();
-            ViewBag.student = studentList;
+            var courseList = context.Courses.ToList();
+            List<SelectListItem> courses = (from x in courseList select new SelectListItem { Text = x.CourseName, Value = x.CourseId.ToString() }).ToList();
+            ViewBag.course = courses;
             return View();
         }
 
         [HttpPost]
-        public ActionResult AddCourseRegister(CourseRegister courseRegister)
+        public ActionResult Index(CourseRegister courseRegister)
         {
+            string student = Session["studentName"].ToString();
+            courseRegister.StudentId= context.Students.Where(x=>x.NameSurname==student).Select(x=>x.StudentId).FirstOrDefault();
             context.CourseRegisters.Add(courseRegister);
             context.SaveChanges();
-            return RedirectToAction("Index");
-        }
 
-        // KURS KAYDI GUNCELLEME ISLEMI
-        [HttpGet]
-        public ActionResult UpdateCourseRegister(int id)
-        {
-            var courses = context.Courses.ToList();
-            List<SelectListItem> courseList = (from y in courses
-                                               select new SelectListItem
-                                               {
-                                                   Text = y.CourseName,
-                                                   Value = y.CourseId.ToString()
-                                               }).ToList();
-            ViewBag.course = courseList;
-
-            var students = context.Students.ToList();
-            List<SelectListItem> studentList = (from x in students
-                                                select new SelectListItem
-                                                {
-                                                    Text = x.UserName,
-                                                    Value = x.StudentId.ToString()
-                                                }).ToList();
-            ViewBag.student = studentList;
-
-            var value = context.CourseRegisters.Find(id);
-            return View(value);
-        }
-
-        [HttpPost]
-        public ActionResult UpdateCourseRegister(CourseRegister courseRegister)
-        {
-            var value = context.CourseRegisters.Find(courseRegister.CourseRegisterId);
-            value.StudentId = courseRegister.StudentId;
-            value.CourseId = courseRegister.CourseId;
-            context.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));  
         }
     }
 }
