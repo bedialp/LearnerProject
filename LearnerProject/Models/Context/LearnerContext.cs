d94e2a0c7e3de0ab97f16de66f50c692a23be864
bedialp <fvzlpktk@gmail.com>
2024-05-09 02:05:45 +0300

    Revert "Admin Table Added"
    This reverts commit 4bc94b63c262e621ee194c1cfd030539c3817fe9.

--- LearnerProject/Models/Context/LearnerContext.cs
+++ LearnerProject/Models/Context/LearnerContext.cs
@@ -7,23 +7,22 @@ using System.Web;
 
 namespace LearnerProject.Models.Context
 {
-	public class LearnerContext : DbContext
-	{
-		//pluralize
-		public DbSet<About> Abouts { get; set; }
-		public DbSet<Admin> Admins { get; set; }
-		public DbSet<Banner> Banners { get; set; }
-		public DbSet<Category> Categories { get; set; }
-		public DbSet<Classroom> Classrooms { get; set; }
-		public DbSet<Course> Courses { get; set; }
-		public DbSet<CourseRegister> CourseRegisters { get; set; }
-		public DbSet<FAQ> FAQs { get; set; }
-		public DbSet<Review> Reviews { get; set; }
-		public DbSet<Student> Students { get; set; }
-		public DbSet<Testimonial> Testimonials { get; set; }
-		public DbSet<Teacher> Teachers { get; set; }
-		public DbSet<CourseVideo> CourseVideos { get; set; }
-		public DbSet<Contact> Contacts { get; set; }
-		public DbSet<Message> Messages { get; set; }
-	}
+    public class LearnerContext :DbContext
+    {
+        //pluralize
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Classroom> Classrooms { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseRegister> CourseRegisters { get; set; }
+        public DbSet<FAQ> FAQs { get; set; }
+        public DbSet<Review> Reviews{ get; set; }
+        public DbSet<Student> Students{ get; set; }
+        public DbSet<Testimonial> Testimonials{ get; set; }
+        public DbSet<Teacher> Teachers{ get; set; }
+        public DbSet<CourseVideo> CourseVideos { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
 }
