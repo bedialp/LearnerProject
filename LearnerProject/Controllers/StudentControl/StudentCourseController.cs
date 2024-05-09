6b180ecfa7d31092d677136d87a45cf5ea1566e1
bedialp <fvzlpktk@gmail.com>
2024-05-10 01:18:29 +0300

    Student

--- LearnerProject/Controllers/StudentControl/StudentCourseController.cs
+++ LearnerProject/Controllers/StudentControl/StudentCourseController.cs
@@ -1,4 +1,6 @@
 ﻿using LearnerProject.Models.Context;
+using LearnerProject.Models.Entities;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,21 +9,32 @@ using System.Web.Mvc;
 
 namespace LearnerProject.Controllers
 {
-    public class StudentCourseController : Controller
-    {
-        LearnerContext context = new LearnerContext();
-        public ActionResult Index()
-        {
-            string studentName = Session["studentName"].ToString();
-            var student = context.Students.Where(x => x.NameSurname == studentName).Select(x => x.StudentId).FirstOrDefault();
-            var values = context.CourseRegisters.Where(x => x.StudentId == student).ToList();
-            return View(values);
-        }
+	public class StudentCourseController : Controller
+	{
+		LearnerContext context = new LearnerContext();
+		public ActionResult Index(int pageNumber = 1)
+		{
+			string studentName = Session["studentName"].ToString();
+			var student = context.Students.Where(x => x.NameSurname == studentName).Select(x => x.StudentId).FirstOrDefault();
+			var values = context.CourseRegisters.Where(x => x.StudentId == student).ToList();
+			return View(values);
+		}
 
-        public ActionResult MyCourseList(int id)
-        {
-            var values = context.CourseVideos.Where(x => x.CourseId == id).ToList();
-            return View(values);
-        }
-    }
+		public ActionResult MyCourseList(int id, int pageNumber = 1)
+		{
+			var values = context.CourseVideos.Where(x => x.CourseId == id).ToList().ToPagedList(pageNumber, 1);
+			return View(values);
+		}
+
+		public ActionResult DeleteMyCourse()
+		{
+			string studentName = Session["studentName"].ToString();
+			var student = context.Students.Where(x => x.NameSurname == studentName).Select(x => x.StudentId).FirstOrDefault();
+			var values = context.CourseRegisters.Where(x => x.StudentId == student).FirstOrDefault();
+
+			context.CourseRegisters.Remove(values);
+			context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
 }
--- LearnerProject/Controllers/StudentControl/StudentRegisterController.cs
+++ LearnerProject/Controllers/StudentControl/StudentRegisterController.cs
@@ -8,21 +8,21 @@ using System.Web.Mvc;
 
 namespace LearnerProject.Controllers
 {
-    public class StudentRegisterController : Controller
-    {
-        LearnerContext context= new LearnerContext();
-        [HttpGet]
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class StudentRegisterController : Controller
+	{
+		LearnerContext context = new LearnerContext();
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public ActionResult Index(Student student)
-        {
-            context.Students.Add(student);
-            context.SaveChanges();
-            return RedirectToAction("Index","StudentLogin");
-        }
-    }
+		[HttpPost]
+		public ActionResult Index(Student student)
+		{
+			context.Students.Add(student);
+			context.SaveChanges();
+			return RedirectToAction("Index/", "StudentLogin");
+		}
+	}
 }
